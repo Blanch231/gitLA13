3ac7319934fe89e6ccd9a7e188242364f3f6dd7f
blanch231 <jhonlloydblancha@gmail.com>
2024-11-21 20:20:09 +0800

    Initial program

---
+++ README.md
@@ -0,0 +1,11 @@
+# Our first GitHub activity
+
+This is our first GitHub
+activity.
+Members names:
+Blancha, Jhon Lloyd (leader)
+Atuel, Vincent
+Burac, Xennen
+Duran, Luigi
+Cabigon, Alwynn Ronan
+Tarnate, Dondie
---
+++ program.py
@@ -0,0 +1,24 @@
+print("A - Add Record")
+print("B - View Records")
+print("C - Clear All Records")
+print("D - Count Records")
+print("E - Exit")
+
+choice = ""
+
+while choice.upper() != 'D':
+    choice = input("ENTER SELECTION [A, B, C, D or E]: ")
+    if choice.upper() == 'A':
+        print("Add Record")
+        addRec()
+    elif choice.upper() == 'B':
+        print("View Records")
+        viewRec()
+    elif choice.upper() == 'C':
+        print("Clear All Records")
+        clearRec()
+    elif choice.upper() == 'D':
+        print("Count All Records")
+         countRec()
+    elif choice.upper() == 'E':
+        print("Thank you!")
